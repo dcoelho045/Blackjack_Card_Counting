2deccb5f167dfcff06f2eb996e1764a5e47192f8
dcoelho045 <dcoelho045@gmail.com>
2023-05-23 00:22:15 -0700

    52 Cards added, Playscreen layedout visually

--- src/components/CardDealing.js
+++ src/components/CardDealing.js
@@ -1,24 +1,34 @@
-import cardImage from "../images/cards/AS.png"
-
+import dealerCard1 from "../images/cards/AS.png"
+import dealerCard2 from "../images/cards/AH.png"
+import playerCard1 from "../images/cards/2C.png"
+import playerCard2 from "../images/cards/KD.png"
 
 const CardDealing = () => {
+    const baseImageAddressDealer1 = "../images/cards/##.png";
+
     return ( 
         <>
-            {/* <div className="CardBeingDealt">
-                <div style={{cardImage: `url(${cardImage})`, 
-                    backgroundRepeat: "no-repeat", 
-                    backgroundSize: "cover",
-                    height: "500px",
-                    width: "200px"
-                    }}>
-                </div>
-            </div> */}
-
-            <div className="CardBeingDealt" style={{}}>
-                <img style={{ width: 200, height: 280 }}src={cardImage}/>
+            <div className="startButton">
+                <button>Deal Cards</button>
+            </div>
+            <div className="dealtCardDealer1" style={{top: "17%", left: "42.5%"}}>
+                <img style={{ width: 200, height: 280 }}src={dealerCard1} alt="dealerCard1"/>
+            </div>
+            <div className="dealtCardDealer2" style={{top: "22%", left: "45.5%"}}>
+                <img style={{ width: 200, height: 280 }}src={dealerCard2} alt="dealerCard2"/>
+            </div>
+            <div className="dealtPlayerCard1" style={{top: "50%", left: "42.5%"}}>
+                <img style={{ width: 200, height: 280 }}src={playerCard1} alt="playerCard1"/>
+            </div>
+            <div className="dealtPlayerCard2" style={{top: "55%", left: "45.5%"}}>
+                <img style={{ width: 200, height: 280 }}src={playerCard2} alt="playerCard2"/>
             </div>
         </>
     );
 }
  
 export default CardDealing;
+
+// check - import the 52 images of the cards
+//       - create a string that can be concatenated so that random cards can be selected
+//       - figure out how to hide and then show two sets of hands and their layouts
--- src/components/PlayPage.js
+++ src/components/PlayPage.js
@@ -4,19 +4,6 @@ import CardDealing from "./CardDealing";
 const PlayPage = () => {
     return ( 
         <>
-            {/* <div className="backgroundImagePage">
-                <div style={{backgroundImage: `url(${backgroundImage})`, 
-                    backgroundRepeat: "no-repeat", 
-                    backgroundSize: "cover",
-                    height: "100vh"
-                    }}>
-                    <div>
-                        <BackButton/>
-                        <CardDealing/>
-                    </div>
-                </div>
-            </div> */}
-
             <div className="main-container">
                 <CardDealing/>
                 <BackButton/>
